43ef3e4315025eba5319cd131ec7654e507fd92d
Jonathan Rosa <azukijra@gmail.com>
2025-12-17 20:32:10 +0100

    Entornos

---
+++ src/test/java/com/azahartech/eventdev/util/UtilidadValidacionTest.java
@@ -0,0 +1,12 @@
+package com.azahartech.eventdev.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilidadValidacionTest {
+
+    @Test
+    void esEmailValido() {
+    }
+}
